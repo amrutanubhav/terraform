988ccdc25730a0edb27f2775738e9fad9bf5d308
Amrut Anubhav <anubhavamrut@gmail.com>
2023-02-11 20:37:01 +0530

    terraform basics

--- 01-outputs/demo.tf
+++ 01-outputs/demo.tf
@@ -22,3 +22,18 @@ output "printing-demo2" {
 
 }
 
+variable "example-list" {
+
+ default = [
+    "aws" ,
+    "devops" ,
+    100 ,
+    true
+ ]
+  
+}
+
+output "printing ist vars" {
+
+    value = "welcome to ${var.example-list[0] ${var.example-list[1] training, course duration is ${var.example-list[2] hours and its${var.example-list[3] }"
+}
