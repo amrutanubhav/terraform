6ced7fb21e9906572cd6c0db6dd19ab144ce9f42
Amrut Anubhav <anubhavamrut@gmail.com>
2023-02-12 18:23:28 +0530

    terraform basics

--- 03-ec2/ec2.tf
+++ 03-ec2/ec2.tf
@@ -27,12 +27,21 @@ resource "aws_instance" "this" {
 
 resource "aws_security_group" "allow_ssh" {
   name        = "allow_ssh"
-  description = "Allow ssh inbound traffic"
+  description = "Allow ssh,http inbound traffic"
 
   ingress {
     description      = "ssh"
     from_port        = 22
     to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    
+  }
+
+  ingress {
+    description      = "http"
+    from_port        = 80
+    to_port          = 80
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
     
