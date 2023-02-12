f206af7e869b33aa955051014358751d1b47af96
Amrut Anubhav <anubhavamrut@gmail.com>
2023-02-12 14:53:51 +0530

    terraform basics

--- 03-ec2/ec2.tf
+++ 03-ec2/ec2.tf
@@ -14,6 +14,6 @@ provider "aws" {
 
 # creates an ec2 instance
 resource "aws_instance" "this" {
-  ami                     = "ami-00ff427d936335825"
+  ami                     = "ami-00e87074e52e6c9f9"
   instance_type           = "t3.micro"
 }
