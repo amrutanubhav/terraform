629f128336d53ac8565556d3472c5aeacd7c24ee
Amrut Anubhav <anubhavamrut@gmail.com>
2023-02-12 18:09:54 +0530

    terraform basics

--- 03-ec2/ec2.tf
+++ 03-ec2/ec2.tf
@@ -16,9 +16,37 @@ provider "aws" {
 resource "aws_instance" "this" {
   ami                     = "ami-0c1d144c8fdd8d690" # other ami is ami-00e87074e52e6c9f9
   instance_type           = "t2.micro" # t3.medium
+  vpc_security_group_ids  = [aws_security_group.allow_ssh.id]
 
   tags = {
     Name = "Test server"
+  }
+}
+
+#security group create
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow ssh inbound traffic"
+
+  ingress {
+    description      = "ssh"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh_all"
   }
 }
 
