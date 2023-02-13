59100f3dcf3aa8d6f3d2b35e711e077989c994fa
Amrut Anubhav <anubhavamrut@gmail.com>
2023-02-13 16:26:01 +0530

    terraform basics

--- 05-modules/ec2/main.tf
+++ 05-modules/ec2/main.tf
@@ -1,6 +1,6 @@
 # creates an ec2 instance
 resource "aws_instance" "this" {
-  ami                     = "data.aws_ami.ami_id" # other ami is ami-00e87074e52e6c9f9
+  ami                     = "data.aws_ami.ami_id.image_id" # other ami is ami-00e87074e52e6c9f9
   instance_type           = "t2.micro" # t3.medium
   vpc_security_group_ids  = [var.sg]
 
