9eeaacce7bd84664a350e3889640e5b45681af99
Amrut Anubhav <anubhavamrut@gmail.com>
2023-02-13 17:55:25 +0530

    terraform basics

--- 05-modules/ec2/data.tf
+++ 05-modules/ec2/data.tf
@@ -1,5 +1,6 @@
 data "aws_ami" "image" {
   most_recent      = true
-  name_regex       = "DevOps-LabImage-CentOS7"
+  name_regex       = "b52-ansible-dev-20Jan2023"
+  owners = ["355449129696"]
 }
 
