1512c57fe32bba085c66c6cdf2a36487a564f167
Amrut Anubhav <anubhavamrut@gmail.com>
2023-02-13 17:16:03 +0530

    terraform basics

--- 05-modules/ec2/main.tf
+++ 05-modules/ec2/main.tf
@@ -1,6 +1,6 @@
 # creates an ec2 instance
 resource "aws_instance" "this" {
-  ami                     = data.aws_ami.lab-image.image_id 
+  ami                     = data.aws_ami.image.image_id 
   instance_type           = "t2.micro" # t3.medium
   vpc_security_group_ids  = [var.sg]
 
