9053f03a09cf1cf7e22b892196aece3a3b6e8618
Amrut Anubhav <anubhavamrut@gmail.com>
2023-03-10 13:43:22 +0530

    updating

--- 05-modules/ec2/main.tf
+++ 05-modules/ec2/main.tf
@@ -4,11 +4,24 @@ resource "aws_instance" "this" {
   instance_type           = "t2.micro" # t3.medium
   vpc_security_group_ids  = [var.sg]
 
-  tags = {
-    Name = "Terraform server"
+  provisioner "remote-exec" {
+
+        connection {
+          type     = "ssh"
+          user     = "centos"
+          password = "DevOps321"
+          host     = self.public_ip
+        }
+
+              inline = [
+                "ansible-pull -U "
+              ]
+
+              tags = {
+                Name = "Terraform server"
+              }
   }
 }
-
 variable "sg" {}
 
 output "public_dns" {
