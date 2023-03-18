3bd3f8b2ad16ad3e99120996e616f9288901451e
Amrut Anubhav <anubhavamrut@gmail.com>
2023-03-18 15:44:22 +0530

    updating

--- 05-modules/ec2/main.tf
+++ 05-modules/ec2/main.tf
@@ -3,6 +3,9 @@ resource "aws_instance" "this" {
   ami                     = data.aws_ami.image.image_id 
   instance_type           = "t2.micro" # t3.medium
   vpc_security_group_ids  = [var.sg]
+  tags = {
+    Name = "Terraform server"
+ }
 
   provisioner "remote-exec" {
 
@@ -17,9 +20,7 @@ resource "aws_instance" "this" {
                 "ansible-pull -U "
               ]
 
-              tags = {
-                Name = "Terraform server"
-              }
+ 
   }
 }
 variable "sg" {}
